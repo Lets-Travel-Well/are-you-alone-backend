71298e9c252c3a6f5e72a2448f6169b8a0f5e8e6
KCH <doolysmile1@naver.com>
2023-04-15 18:37:46 +0900

    chore : create, modified date를 위한 설정

--- src/main/java/com/rualone/app/AppApplication.java
+++ src/main/java/com/rualone/app/AppApplication.java
@@ -2,8 +2,11 @@ package com.rualone.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AppApplication {
 
 	public static void main(String[] args) {
