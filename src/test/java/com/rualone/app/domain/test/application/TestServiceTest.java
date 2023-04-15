af61dec929825781049e572a228714b1f640cb82
KCH <doolysmile1@naver.com>
2023-04-15 18:38:14 +0900

    chore : 참고용 테스트용 참고 CREATE 구현

---
+++ src/test/java/com/rualone/app/domain/test/application/TestServiceTest.java
@@ -0,0 +1,10 @@
+package com.rualone.app.domain.test.application;
+
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+class TestServiceTest {
+
+}
