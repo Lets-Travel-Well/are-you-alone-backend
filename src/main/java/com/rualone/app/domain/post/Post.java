bdff9322c2a83feebb89938e0505961b9b23b1c1
KCH <doolysmile1@naver.com>
2023-04-20 02:18:56 +0900

    feat : post 엔티티

---
+++ src/main/java/com/rualone/app/domain/post/Post.java
@@ -0,0 +1,20 @@
+package com.rualone.app.domain.post;
+
+import com.rualone.app.global.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+public class Post extends BaseEntity {
+    private String subject;
+    private String content;
+    private int hit;
+    // TODO : member 추가되면 추가 시켜야함
+//    private Member member;
+}
