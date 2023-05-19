46ce1960f1ede11b8c87b5cd24296820ea88fb32
KCH <doolysmile1@naver.com>
2023-05-19 15:56:16 +0900

    refactor : post Validator 추가
    좋아요를 구현하기 위해 서비스 단 간의 의존성을 줄이기 위해 post validator 추가

---
+++ src/main/java/com/rualone/app/domain/board/validator/PostValidator.java
@@ -0,0 +1,18 @@
+package com.rualone.app.domain.board.validator;
+
+import com.rualone.app.domain.board.dao.PostRepository;
+import com.rualone.app.domain.board.entity.Post;
+import com.rualone.app.global.error.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.NoSuchElementException;
+
+@Configuration
+@RequiredArgsConstructor
+public class PostValidator {
+    private final PostRepository postRepository;
+    public Post findById(Long id){
+        return postRepository.findById(id).orElseThrow(() -> new NotFoundException(Post.class, id));
+    }
+}
