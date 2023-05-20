b8e78925275713ae7dcd9b4edd8c9f0af2d3e1e4
KCH <doolysmile1@naver.com>
2023-05-20 19:52:40 +0900

    chore : journey 작업중

--- src/main/java/com/rualone/app/domain/journey/api/JourneyApi.java
+++ src/main/java/com/rualone/app/domain/journey/api/JourneyApi.java
@@ -1,11 +1,25 @@
 package com.rualone.app.domain.journey.api;
 
+import com.rualone.app.domain.journey.application.JourneyService;
+import com.rualone.app.global.api.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.rualone.app.global.api.ApiResult.OK;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/journey-management")
 public class JourneyApi {
+    private JourneyService journeyService;
+
+    @Operation(summary = "journey 생성", description = "journey를 생성하는 API입니다.")
+    @PostMapping("/create")
+    public ApiResult<Void> saveJourney(@RequestBody ){
+        return OK(null);
+    }
 }
--- src/main/java/com/rualone/app/domain/journey/application/impl/JourneyServiceImpl.java
+++ src/main/java/com/rualone/app/domain/journey/application/impl/JourneyServiceImpl.java
@@ -1,6 +1,16 @@
 package com.rualone.app.domain.journey.application.impl;
 
 import com.rualone.app.domain.journey.application.JourneyService;
+import com.rualone.app.domain.journey.dao.JourneyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional
 public class JourneyServiceImpl implements JourneyService {
+    private final JourneyRepository journeyRepository;
 }
