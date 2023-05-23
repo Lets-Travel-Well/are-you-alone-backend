b068a1ef9420c8e53dd87efef01dbeb6b34a9cfc
marugy <gyulife7301@naver.com>
2023-05-23 14:30:50 +0900

    chore : 내 댓글인지

--- src/main/java/com/rualone/app/domain/board/application/impl/PostQueryServiceImpl.java
+++ src/main/java/com/rualone/app/domain/board/application/impl/PostQueryServiceImpl.java
@@ -9,6 +9,7 @@ import com.rualone.app.domain.board.validator.PostValidator;
 import com.rualone.app.domain.member.entity.Member;
 import com.rualone.app.domain.member.validator.MemberValidator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,7 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class PostQueryServiceImpl implements PostQueryService {
     private final PostQueryRepository postQueryRepository;
     private final PostValidator postValidator;
@@ -33,6 +35,14 @@ public class PostQueryServiceImpl implements PostQueryService {
 
         postDetailResponse.setMyBoard(member.getNickName().equals(postDetailResponse.getAuthorName()));
 
+
+        postDetailResponse.getCommentList().forEach(commentResponse -> {
+            if(commentResponse.getAuthorName().equals(member.getNickName())){
+                log.info("{}",commentResponse);
+                commentResponse.setMyComment(true);
+            }
+        });
+
         post.increaseHit();
         return postDetailResponse;
     }
@@ -41,5 +51,4 @@ public class PostQueryServiceImpl implements PostQueryService {
     public Page<PostResponse> findAll(Pageable pageable) {
         return postQueryRepository.findAll(pageable);
     }
-
 }
--- src/main/java/com/rualone/app/domain/board/dto/response/CommentResponse.java
+++ src/main/java/com/rualone/app/domain/board/dto/response/CommentResponse.java
@@ -14,11 +14,20 @@ public class CommentResponse {
     private String content;
     private String authorName;
     private LocalDateTime createDate;
+    private Boolean myComment;
     public CommentResponse(Comment comment){
         this.id = comment.getId();
         this.postId = comment.getPost().getId();
         this.content = comment.getContent();
         this.authorName = comment.getMember().getNickName();
         this.createDate = comment.getCreateDate();
+    }
+    public CommentResponse(Long id, Long postId, String content, String authorName, LocalDateTime createDate){
+        this.id = id;
+        this.postId = postId;
+        this.content = content;
+        this.authorName = authorName;
+        this.createDate = createDate;
+        this.myComment = false;
     }
 }
