2fc134fa248f3e0cbbcb6650adcc454dc4a195f0
marugy <gyulife7301@naver.com>
2023-05-23 20:16:10 +0900

    feat : 여행지에 대한 좋아요 상태 변경

--- src/main/java/com/rualone/app/domain/hotplace/api/HotPlaceApi.java
+++ src/main/java/com/rualone/app/domain/hotplace/api/HotPlaceApi.java
@@ -5,8 +5,11 @@ import com.rualone.app.domain.hotplace.application.HotPlaceService;
 import com.rualone.app.domain.hotplace.dto.response.HotPlaceResponse;
 import com.rualone.app.global.api.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +28,8 @@ public class HotPlaceApi {
     private final HotPlaceQueryService hotPlaceQueryService;
     @Operation(summary = "attractionInfo 좋아요", description = "attractionInfo 좋아요 API입니다. True시 좋아요를 누른 상태 False시 취소한 상태입니다.")
     @GetMapping("/{id}/like")
-    public ApiResult<Boolean> changLike(@PathVariable("id") Integer contentId){
-        Long memberId = 1L;
+    public ApiResult<Boolean> changLike(@PathVariable("id") Integer contentId, @Parameter(hidden = true) @AuthenticationPrincipal User user){
+        Long memberId = Long.parseLong(user.getUsername());
         return OK(hotPlaceService.changeHotPlace(memberId, contentId));
     }
     @Operation(summary = "hotplace 보기", description = "hotplace 상위 10개 보여주는 API입니다.")
