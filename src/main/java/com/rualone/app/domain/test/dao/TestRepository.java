174ca8416f50f1534b933674da90a8ff0ffc5497
KCH <doolysmile1@naver.com>
2023-05-25 17:41:31 +0900

    refactor : Test 도메인 삭제

--- src/main/java/com/rualone/app/domain/test/dao/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rualone.app.domain.test.dao;
-
-import com.rualone.app.domain.test.domain.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
