d45aa3af92697eaf3137438b9f7f5db546162174
KCH <doolysmile1@naver.com>
2023-06-23 10:30:29 +0900

    fix : 조건문 수정

--- src/main/java/com/rualone/app/domain/journey/dao/JourneyQueryRepository.java
+++ src/main/java/com/rualone/app/domain/journey/dao/JourneyQueryRepository.java
@@ -81,7 +81,7 @@ public class JourneyQueryRepository {
                         journey.leader.id.eq(memberId)
                 )
                 .fetchOne();
-        return fetchOne == null;
+        return fetchOne != null;
     }
 
     public List<JourneyPlaceResponse> findJourneyPlace(Long journeyId) {
